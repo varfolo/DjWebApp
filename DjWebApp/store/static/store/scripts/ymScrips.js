849fd366d2f8acbd483fae7fa7577b872a36bf54
Константин <@KONSTANTIN>
2019-02-10 23:38:20 +0300

    Скрипты YnadexMaps убраны в отдельный файл.

---
+++ DjWebApp/store/static/store/scripts/ymScrips.js
@@ -0,0 +1,18 @@
+ymaps.ready(init);
+
+
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+        center: [55.76, 37.64],
+        zoom: 7
+    });
+    var myGeoObject = new ymaps.GeoObject({
+        geometry: {
+            type: "Point", // тип геометрии - точка 
+            coordinates: [55.9, 37.8] // координаты точки
+        }
+    });
+    // Размещение геообъекта на карте.
+    myMap.geoObjects.add(myGeoObject);
+}
